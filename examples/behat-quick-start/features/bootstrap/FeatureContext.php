1767464925e08b43973a9568a82871ccb60f8dd3
YMHuang <ym.huang0808@gmail.com>
2016-10-07 11:33:39 +0800

    Add test cases in examples/behat-quick-start/features/FeatureContext.php

--- examples/behat-quick-start/features/bootstrap/FeatureContext.php
+++ examples/behat-quick-start/features/bootstrap/FeatureContext.php
@@ -3,6 +3,7 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
 
 /**
  * Defines application features from the specific context.
@@ -18,5 +19,45 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given there is a(n) :arg1, which costs $:arg2
+     */
+    public function thereIsAWhichCosts($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add the :arg1 to the basket
+     */
+    public function iAddTheToTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 product in the basket
+     */
+    public function iShouldHaveProductInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall basket price should be $:arg1
+     */
+    public function theOverallBasketPriceShouldBe($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 products in the basket
+     */
+    public function iShouldHaveProductsInTheBasket($arg1)
+    {
+        throw new PendingException();
     }
 }
